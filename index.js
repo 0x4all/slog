2a31bf13b57e60a64a3db1f09dadee306d66c9ba
0x4all <steven.2018@qq.com>
2018-12-30 21:16:01 +0800

    add begin

--- index.js
+++ index.js
@@ -1,6 +1,80 @@
 var pomelologger = require("pomelo-logger");
-module.exports = {
-	getLogger: pomelologger.getLogger,
+
+
+var getLogger = function(categoryName, fileName){
+    var logger = pomelologger.getLogger(categoryName, fileName);
+    _add_begin(logger);
+    return logger;
+};
+
+var _add_begin = function(logger)
+{
+    logger.begin = function()
+    {
+        var str = new Error().stack;
+        var startIndex1 = str.indexOf('(');
+        startIndex1 = str.indexOf('at',startIndex1 + 1);
+        str = str.substring(startIndex1+3,str.indexOf('(',startIndex1+2)) + "[  ";
+
+        var args = Array.prototype.slice.call(arguments,0);
+        for(var i = 0,l = args.length; i < l; ++i )
+        {
+            var arg = args[i];
+            var strArg = arg + "";
+            if(strArg === '[object Object]')
+            {
+                str += JSON.stringify(arg)+ ", ";
+            }
+            else if(strArg === '[object Arguments]')
+            {
+                str += _args2String(arg);
+            }
+            else
+            {
+                str += strArg + ", ";
+            }
+        }
+        //str = str.substr(0,str.length - 2);
+        str += " ];";
+
+        logger.trace(str);
+    };
+
+    return logger;
+};
+
+var _args2String = function(args)
+{
+    var str = "";
+    for(var arg in args)
+    {
+        if(args.hasOwnProperty(arg))
+        {
+            var s = args[arg] + "";
+            if (s === '[object Object]')
+            {
+                str +=  JSON.stringify(args[arg]) + "|";
+            }
+            else if (s === '[object Arguments]')
+            {
+                str +=  JSON.stringify(args[arg]) + "|";
+            }
+            else
+            {
+                str += s + "|";
+            }
+        }
+    }
+    str = str.substr(0,str.length - 1);
+    str += "";
+    return str;
+};
+
+
+
+
+var logger = {
+	getLogger: getLogger,
 	getDefaultLogger: pomelologger.getDefaultLogger,
 
 	addAppender: pomelologger.addAppender,
@@ -17,3 +91,5 @@ module.exports = {
 	layouts: pomelologger.layouts,
 	appenders: pomelologger.appenders
 };
+
+module.exports = logger;
